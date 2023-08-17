c9da45b7e495f31beb6c4f1048509e336a6fbef2
4t-2 <zbfddx@protonmail.com>
2023-08-17 23:43:46 +0100

    Made basic project structure

---
+++ README.md
@@ -0,0 +1,10 @@
+# The Endless Show
+A show written by AI (chatGPT) that writes its own story and characters.
+
+## Script Prompt
+```
+Create a script that follows these rules
+- rule 1
+- rule 2
+- rule 3
+```
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+class StageDirections
+{
+};
+
+class SceneChange
+{
+};
+
+class Dialogue
+{
+};
+
+union Element {
+};
+
+class Script
+{
+};
+
+int main()
+{
+	printf("Hello World\n");
+}
